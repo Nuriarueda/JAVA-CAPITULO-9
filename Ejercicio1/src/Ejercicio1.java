de2403622e376bc10550b2f3f1d569d673b1c474
Nuriarueda <ruedareinanuria@gmail.com>
2024-01-29 10:05:52 +0100

    capitulo 9

--- Ejercicio1/src/Ejercicio1.java
+++ Ejercicio1/src/Ejercicio1.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 public class Ejercicio1 {
     public static void main(String[] args) {
+
         ArrayList<String> a = new ArrayList<String>();
+
         System.out.println("Nº de elementos: " + a.size());
+
         a.add("rojo");
         a.add("verde");
         a.add("azul");
+
         System.out.println("Nº de elementos: " + a.size());
+
         a.add("blanco");
+
         System.out.println("Nº de elementos: " + a.size());
         System.out.println("El elemento que hay en la posición 0 es " + a.get(0));
         System.out.println("El elemento que hay en la posición 3 es " + a.get(3));
+        
     }
 }
+
