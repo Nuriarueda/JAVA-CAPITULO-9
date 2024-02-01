9f49d4ed28993268543adcc3e9a45fd49edb2115
Nuriarueda <ruedareinanuria@gmail.com>
2024-02-01 08:26:14 +0100

    capitulo 9

--- ECENTRO/src/ecentro/Usuario.java
+++ ECENTRO/src/ecentro/Usuario.java
@@ -1,10 +1,28 @@
 package ecentro;
 
 public class Usuario {
+    private int id;
     private String nombre;
     private String apellidos;
     private String email;
     private String password;
+    
 
+
+    Usuario(String nombre, String appellidos, String email, String password){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.password = password;
+
+    }
+    public String getNombre(){
+        return nombre;
+        
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
 
 }
